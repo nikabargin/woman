247372a0ff0b6734917885d6cf23eb58f84c9b9c
Nikita <nikabargin@gmail.com>
2019-06-04 12:11:31 +0300

    Утро нового дня

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require jquery-ui/widget
 //= require jquery-ui/sortable
 //= require activestorage
