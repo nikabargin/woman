270ba5eed3f464a05321411cbec840972845187d
Nikita <nikabargin@gmail.com>
2019-06-04 12:45:14 +0300

    Утро нового дня 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.serve_static_assets = false
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
